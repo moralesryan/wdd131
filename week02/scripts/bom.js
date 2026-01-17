955a10bac1ea721d18d1d4a8d2c2410103d5f608
moralesryan <rmoralesaguilar@byupathway.edu>
2026-01-17 09:45:15 -0600

    "fixed the js script"

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,18 +1,32 @@
 //declare the variables//
-const input = DocumentquerySelector('#favchav');
-const button = DocumentquerySelector('button');
-const list = DocumentquerySelector('____');
-//create a li element and a button//
-const titleHolder = document.createElement('li');
-const deleteButton = document.createElement('button')
-//append info to the new li element//
-list.textContent = input.value;
-deleteButton.textContent = '❌';
-titleHolder.append(deleteButton);
-list.append(titleHolder);
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
 //Event for the Add chapter button//
-button.addEventListener("click", function() {
-    if (input.value.trim() !== '') {
-        console.log("thanks") //comeback to this//
+button.addEventListener('click', () => {
+    if (input.value.trim() === '') {
+        alert('Please enter a chapter name');
+        input.focus() //comeback to this//
+        return;
     }
+    //create a li element and a button//
+    const titleHolder = document.createElement('li');
+    const deleteButton = document.createElement('button')
+
+    //append info to the new li element//
+    titleHolder.textContent = input.value;
+    deleteButton.textContent = '❌';
+    //append delete button to li 
+    titleHolder.append(deleteButton);
+    list.append(titleHolder);
+    //delete button to remove the li elements when clicked 
+    deleteButton.addEventListener('click',() => {
+        list.removeChild(titleHolder); 
+        input.focus();
+    });
+//clean up the interface
+input.value = '';
+//cursor back to the input field 
+input.focus();
 });
